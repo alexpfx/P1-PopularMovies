297d9415fa644be1f4ee5bbf87d059d3fec35003
Alexandre Alessi <alexpfba@gmail.com>
2017-01-14 14:38:08 -0200

    implementacao okhttp

--- app/src/main/java/udacity/nanodegree/android/p1/network/fetch/impl/OkHttpFetcher.java
+++ app/src/main/java/udacity/nanodegree/android/p1/network/fetch/impl/OkHttpFetcher.java
@@ -2,7 +2,15 @@ package udacity.nanodegree.android.p1.network.fetch.impl;
 
 import android.content.Context;
 import android.net.Uri;
+import android.util.Log;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import udacity.nanodegree.android.p1.network.fetch.AbstractMovieDbFetcher;
 import udacity.nanodegree.android.p1.network.fetch.MovieDbFetcher;
 import udacity.nanodegree.android.p1.network.fetch.MovieDbResponseListener;
@@ -12,7 +20,9 @@ import udacity.nanodegree.android.p1.network.fetch.MovieDbUriComposer;
  * Created by alexandre on 14/01/2017.
  */
 
-public class OkHttpFetcher extends AbstractMovieDbFetcher implements MovieDbFetcher {
+public class OkHttpFetcher extends AbstractMovieDbFetcher implements MovieDbFetcher, Callback {
+    private static final String TAG = "OkHttpFetcher";
+
     protected OkHttpFetcher(Context context,
             MovieDbUriComposer movieDbUriComposer,
             MovieDbResponseListener responseListener) {
@@ -21,6 +31,23 @@ public class OkHttpFetcher extends AbstractMovieDbFetcher implements MovieDbFetc
 
     @Override
     protected void doFetch(Uri uri) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(uri.toString()).build();
+
+        client.newCall(request).enqueue(this);
+
+    }
+
+    @Override
+    public void onFailure(Call call, IOException e) {
+        getErrorListener().onError("network error ", call, e);
+    }
+
+    @Override
+    public void onResponse(Call call, Response response) throws IOException {
+        if (!response.isSuccessful()) onFailure(call, new IOException(response.message()));
+
+        Log.d(TAG, "onResponse: " + response.body().toString());
 
     }
 }
